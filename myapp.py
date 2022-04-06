26015edd8e854dc5d44a3cf4bf61d15f70876095
xz90 <xz90@illinois.edu>
2022-04-05 23:01:49 -0500

    version2.1 empty trial

--- myapp.py
+++ myapp.py
@@ -8,7 +8,6 @@ def connect_db():
     sql.row_factory = sqlite3.Row
     return sql
 
-
 def get_db():
     #Check if DB is there
     if not hasattr(g, 'sqlite3'):
@@ -21,7 +20,6 @@ def close_db(error):
     #if global object has a sqlite database then close it. If u leave it open noone can access it and gets lost in memory causing leaks.
     if hasattr(g, 'sqlite_db'):
         g.sqlite3_db.close()
-
 
 @app.route('/')
 def viewusers():
