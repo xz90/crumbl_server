65a19b42edf45327cc961fabbce4c7dbd583797f
sharonhsl <hslam2@illinois.edu>
2022-04-19 18:54:50 -0500

    look for domains in tracker_domains

--- myapp.py
+++ myapp.py
@@ -27,32 +27,46 @@ def viewusers():
   name = request.form['name']
   domain = request.form['domain']
   if name:
-    # For trackerdb.sql
-    query = "SELECT trackers.name, categories.name as 'category' FROM trackers, categories WHERE trackers.category_id = categories.id and trackers.name = ?"
+    # check whether cookie name exists in open-cookie-database.csv
+    query = "SELECT cookie_data_key_name as 'name', category as 'category' FROM open_database WHERE cookie_data_key_name = ?"
     cursor = db.execute(query, (name, ))
     results = cursor.fetchall()
-    if len(results) > 0:
-      category = results[0]['category']
-      return jsonify(
-        name = name,
-        domain = domain,
-        category = category
-      )
-    else:
-      # For open-cookie-database.csv
-      query1 = "SELECT cookie_data_key_name as 'name', category as 'category' FROM open_database WHERE cookie_data_key_name = ?"
-      cursor1 = db.execute(query1, (name, ))
-      results = cursor1.fetchall()
-      if len(results) > 0: 
+    if len(results) > 0: 
         category = results[0]['category']
         return jsonify(
           name = name,
           domain = domain,
           category = category
         )
-  # For WhoTracksMe sites
+
+  # check whether cookie domain exists in tracker_domains table
   if domain:
-    domaincopy = domain
+    query = """
+    SELECT categories.id, categories.name, trackers.id, tracker_domains.domain
+    FROM trackers
+    JOIN categories ON categories.id = trackers.category_id
+    JOIN tracker_domains ON tracker_domains.tracker = trackers.id
+    WHERE domain = ?
+    """
+    while len(domain) != 0:
+      cursor = db.execute(query, (domain, ))
+      results = cursor.fetchall()
+      if len(results) > 0: 
+        category = results[0]['name']
+        return jsonify(
+          name = name,
+          domain = domain,
+          category = category
+        )
+      else:
+        dlist = domain.split('.', 1)
+        if len(dlist) > 1:
+          domain = domain.split('.', 1)[1]
+        else:
+          break
+
+    # check whether cookie domain exists in sites table
+    domain = request.form['domain']
     query = "SELECT * FROM sites WHERE domain = ?"
     while len(domain) != 0:
       cursor = db.execute(query, (domain, ))
@@ -67,12 +81,12 @@ def viewusers():
       else:
         dlist = domain.split('.', 1)
         if len(dlist) > 1:
-          domain = dlist[1]
+          domain = domain.split('.', 1)[1]
         else:
           break
   return jsonify(
         name = name,
-        domain = domaincopy,
+        domain = domain,
         category = "unknown"
       )
 
